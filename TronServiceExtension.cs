abda345821c1635375e32bae5683cf76a57d53b6
jingkunlee <130815413+jingkunlee@users.noreply.github.com>
2024-08-08 08:23:19 +0800

    Update TronServiceExtension.cs

--- TronServiceExtension.cs
+++ TronServiceExtension.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Options;
 
 namespace Tron.Exchange;
 
-using TronNetSdk;
+using Tron;
 
 public record TronRecord(IServiceProvider ServiceProvider, ITronClient? TronClient, IOptions<TronNetOptions>? Options);
 
